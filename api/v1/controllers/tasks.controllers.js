fdc6bfd6fd5db429787478b564daaa57c8cdd0f3
Ho Hung <hohung1203@gmail.com>
2024-02-20 13:26:01 +0700

    add func sort focus key

--- api/v1/controllers/tasks.controllers.js
+++ api/v1/controllers/tasks.controllers.js
@@ -5,6 +5,7 @@ const TaskModel = require('../models/tasks.model')
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
   let filter  = {}
+  let sort = {}
   if (req.query.status) {
     const status = req.query.status
 
@@ -14,12 +15,24 @@ module.exports.index = async (req, res) => {
 
   }
 
+  // START SORT
+
+  if (req.query.sortKey && req.query.sortValue) {
+    const value = req.query.sortValue
+    const sortKey = req.query.sortKey
+
+    sort[sortKey] = value
+
+  }
+
+  // END SORT
+
   const tasks = await TaskModel.find(
     filter,
     {
       deleted: false
     }
-  )
+  ).sort(sort)
   
   res.json(tasks)
 }
