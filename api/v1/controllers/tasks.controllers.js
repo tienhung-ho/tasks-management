689151783ad85ae97540e1e0075907b5d7f238bb
Ho Hung <hohung1203@gmail.com>
2024-02-20 13:17:08 +0700

    add func filter focus on status

--- api/v1/controllers/tasks.controllers.js
+++ api/v1/controllers/tasks.controllers.js
@@ -4,9 +4,22 @@ const TaskModel = require('../models/tasks.model')
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
-  const tasks = await TaskModel.find({
-    deleted: false
-  })
+  let filter  = {}
+  if (req.query.status) {
+    const status = req.query.status
+
+    filter = {
+      status,
+    }
+
+  }
+
+  const tasks = await TaskModel.find(
+    filter,
+    {
+      deleted: false
+    }
+  )
   
   res.json(tasks)
 }
@@ -27,4 +40,6 @@ module.exports.detail = async (req, res) => {
     res.json('404 Not found')
   }
 }
+
+// [GET]
 
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -12,4 +12,5 @@ router.get('/', taskController.index)
 
 router.get('/detail/:id', taskController.detail)
 
+
 module.exports = router
